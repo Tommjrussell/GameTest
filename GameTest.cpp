62d049f76d7c44462c490a34ef22effdb23e7d51
Tom <tom.russell+git@groundfire.net>
2019-02-26 23:13:40 +0000

    Commit a simple CMake project

---
+++ GameTest.cpp
@@ -0,0 +1,12 @@
+﻿// GameTest.cpp : Defines the entry point for the application.
+//
+
+#include "GameTest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
