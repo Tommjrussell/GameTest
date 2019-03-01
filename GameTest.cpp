1f488fedf3a86b8dc1dc239d239a5a218843b29e
Tom Russell <tom.russell+git@groundfire.net>
2019-03-01 17:38:34 +0000

    Integrate GLFW library into project

--- GameTest.cpp
+++ GameTest.cpp
@@ -1,11 +1,14 @@
-﻿// GameTest.cpp : Defines the entry point for the application.
-//
-
-#include "GameTest.h"
+﻿#include "GameTest.h"
+#include "glfw/glfw3.h"
 
 int main()
 {
+	glfwInit();
+
 	std::cout << "Version " << GameTest_VERSION_MAJOR << "." << GameTest_VERSION_MINOR << "\n";
 	std::cout << "Hello CMake." << std::endl;
+
+	glfwTerminate();
+
 	return 0;
 }
