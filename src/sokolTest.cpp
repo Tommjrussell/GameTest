23dd89cfdb9c79a261aa40bf482317bf0c9b8abd
Tom <tom.russell+git@groundfire.net>
2019-03-04 23:12:12 +0000

    improved sokol test

--- src/sokolTest.cpp
+++ src/sokolTest.cpp
@@ -3,18 +3,9 @@
 #include "sokol_app.h"
 
 sg_pass_action pass_action = {0};
-sg_buffer vbuf;
 sg_shader shd;
 sg_pipeline pip;
-sg_bindings binds = {0};
-
-const float vertices[] =
-{
-	// positions			// colors
-	 0.0f,  0.5f, 0.5f,		1.0f, 0.0f, 0.0f, 1.0f,
-	 0.5f, -0.5f, 0.5f,		0.0f, 1.0f, 0.0f, 1.0f,
-	-0.5f, -0.5f, 0.5f,		0.0f, 0.0f, 1.0f, 1.0f
-};
+sg_bindings bind = {0};
 
 void init(void)
 {
@@ -30,55 +21,88 @@ void init(void)
 		sg_setup(&args);
 	}
 
+	const float vertices[] =
 	{
-		sg_buffer_desc args = { 0 };
+		// positions            colors
+		-0.5f,  0.5f, 0.5f,     1.0f, 0.0f, 0.0f, 1.0f,
+		 0.5f,  0.5f, 0.5f,     0.0f, 1.0f, 0.0f, 1.0f,
+		 0.5f, -0.5f, 0.5f,     0.0f, 0.0f, 1.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f,     1.0f, 1.0f, 0.0f, 1.0f,
+	};
+
+	{
+		sg_buffer_desc args = {0};
 		args.size = sizeof(vertices);
 		args.content = vertices;
-		vbuf = sg_make_buffer(&args);
+		bind.vertex_buffers[0] = sg_make_buffer(&args);
+	}
+
+    const uint16_t indices[] = { 0, 1, 2,  0, 2, 3 };
+
+	{
+		sg_buffer_desc args = { 0 };
+		args.type = SG_BUFFERTYPE_INDEXBUFFER;
+		args.size = sizeof(indices);
+		args.content = indices;
+		bind.index_buffer = sg_make_buffer(&args);
 	}
 
 	{
 		sg_shader_desc args = { 0 };
 		args.vs.source =
-			"#version 330\n"
-			"in vec4 position;\n"
-			"in vec4 color0;\n"
-			"out vec4 color;\n"
-			"void main() {\n"
-			"  gl_Position = position;\n"
-			"  color = color0;\n"
-			"}\n";
+R"#~#(
+			#version 330
+
+			in vec4 position;
+			in vec4 color0;
+			out vec4 color;
+
+			void main()
+			{
+				gl_Position = position;
+				color = color0;
+			}
+
+)#~#";
+
 		args.fs.source =
-			"#version 330\n"
-			"in vec4 color;\n"
-			"out vec4 frag_color;\n"
-			"void main() {\n"
-			"  frag_color = color;\n"
-			"}\n";
+R"#~#(
+			#version 330
+
+			in vec4 color;
+			out vec4 frag_color;
+
+			void main()
+			{
+				frag_color = color;
+			}
+
+)#~#";
 		shd = sg_make_shader(&args);
 	}
 
 	{
 		sg_pipeline_desc args = { 0 };
 		args.shader = shd;
+		args.index_type = SG_INDEXTYPE_UINT16;
 		args.layout = { 0 };
 		args.layout.attrs[0].name = "position";
+		args.layout.attrs[0].sem_name = "POS";
 		args.layout.attrs[0].format = SG_VERTEXFORMAT_FLOAT3;
 		args.layout.attrs[1].name = "color0";
+		args.layout.attrs[0].sem_name = "COLOR";
 		args.layout.attrs[1].format = SG_VERTEXFORMAT_FLOAT4;
 
 		pip = sg_make_pipeline(&args);
 	}
-
-	binds.vertex_buffers[0] = vbuf;
 }
 
 void frame(void)
 {
 	sg_begin_default_pass(&pass_action, sapp_width(), sapp_height());
 	sg_apply_pipeline(pip);
-	sg_apply_bindings(&binds);
-	sg_draw(0, 3, 1);
+	sg_apply_bindings(&bind);
+	sg_draw(0, 6, 1);
 	sg_end_pass();
 	sg_commit();
 }
